814995fd0edd7562599fde3545587837a154b659
mizu11pro <mizuakasa@gmail.com>
2021-01-09 08:10:24 +0000

    [修正]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9e049a90cc9948d31aa8f5c518fa8aa9d361302e326576387311bf3107a3f5d9b172c27a26481dcc2422c3408e10ed4cd0d2c8c8fbd2e4f431bbdc60ba20da78'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: :admins do
+    get 'homes' => 'homes#top'
+    resources :homes, only: [:show, :edit, :create, :update, :destroy]
+    # resources :reportimages, only: [:new, :create, :index, :show, :destroy]
+  end
+
+  devise_for :admins, controllers: {
+    sessions:       'admins/sessions',
+    registrations:  'admins/registrations',
+    passwords:      'admins/passwords',
+  }
+
+  scope module: :customers do
+    root 'homes#index'
+  end
+  devise_for :customers, controllers: {
+    sessions:       'customers/sessions',
+    registrations:  'customers/registrations',
+    passwords:      'customers/passwords',
+  }
+
 end
